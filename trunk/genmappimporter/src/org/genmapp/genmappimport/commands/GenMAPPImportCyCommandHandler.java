576bf8f7b9bdb0b37d335042870cf8f4cf5107e1
apico <apico@23aac120-1128-46ae-97c9-9e165d6aae65>
2010-08-24 01:53:41 +0000

    - refactored cycommands - added command for retrieving import source url

--- trunk/genmappimporter/src/org/genmapp/genmappimport/commands/GenMAPPImportCyCommandHandler.java
+++ trunk/genmappimporter/src/org/genmapp/genmappimport/commands/GenMAPPImportCyCommandHandler.java
@@ -15,104 +15,63 @@
  ******************************************************************************/
 package org.genmapp.genmappimport.commands;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+import cytoscape.command.AbstractCommandHandler;
 import cytoscape.command.CyCommandException;
-import cytoscape.command.CyCommandHandler;
+import cytoscape.command.CyCommandManager;
 import cytoscape.command.CyCommandResult;
 import cytoscape.layout.Tunable;
 
 /**
  * CyCommandHandler registration/execution
  * 
- * @author apico
- * 
  */
-public class GenMAPPImportCyCommandHandler implements CyCommandHandler {
+public class GenMAPPImportCyCommandHandler extends AbstractCommandHandler {
 
 	public final static String NAMESPACE = "genmapp importer";
 	public final static String CREATE_NETWORK = "create network";
-	public static boolean CREATE_NETWORK_TOGGLE = false;
+	public final static String ARG_CREATE_NETWORK = "toggle";
+	public final static String IMPORT_SOURCE = "get source";
 
-	private Map<String, List<Tunable>> settings = new HashMap<String, List<Tunable>>();
+	public static boolean CREATE_NETWORK_TOGGLE = false;
+	public static String IMPORT_SOURCE_URL = null;
 
 	public GenMAPPImportCyCommandHandler() {
-		Tunable t = new Tunable("toggle",
-				"Whether to create a network from imported table data",
-				Tunable.STRING, "false");
-		List<Tunable> list = new ArrayList<Tunable>();
-		list.add(t);
-		settings.put(CREATE_NETWORK, list);
+		super(CyCommandManager.reserveNamespace(NAMESPACE));
+
+		addDescription(CREATE_NETWORK,
+				"Set toggle to create network and view from imported table data");
+		addArgument(CREATE_NETWORK, ARG_CREATE_NETWORK);
+
+		addDescription(IMPORT_SOURCE, "get URL for imported table");
+		addArgument(IMPORT_SOURCE);
 	}
 
-	public CyCommandResult execute(String string, Collection<Tunable> clctn)
+	public CyCommandResult execute(String command, Collection<Tunable> args)
 			throws CyCommandException {
-		Map<String, Object> kvSettings = new HashMap<String, Object>();
-		for (Tunable t : clctn) {
-			Object v = t.getValue();
-			if (v != null)
-				kvSettings.put(t.getName(), v.toString());
-			else
-				kvSettings.put(t.getName(), null);
-		}
-		return execute(string, kvSettings);
+		return execute(command, createKVMap(args));
 	}
 
-	public CyCommandResult execute(String command, Map<String, Object> map)
+	public CyCommandResult execute(String command, Map<String, Object> args)
 			throws CyCommandException {
 		CyCommandResult result = new CyCommandResult();
+
+		for (String t : args.keySet()) {
+			result.addMessage("Arg: " + t + " = " + args.get(t));
+		}
 		if (CREATE_NETWORK.equals(command)) {
-			boolean val = Boolean.parseBoolean((String) map.get("toggle"));
+			boolean val = Boolean.parseBoolean((String) args
+					.get(ARG_CREATE_NETWORK));
 			CREATE_NETWORK_TOGGLE = val;
-			result.addMessage("toggle set to " + val);
+		} else if (IMPORT_SOURCE.equals(command)) {
+			result.addResult(IMPORT_SOURCE_URL);
+			result.addMessage("returning URL: " + IMPORT_SOURCE_URL);
 		} else {
 			result.addError("Command not supported: " + command);
 		}
-
-		return result;
+		return (result);
 	}
-
-    public List<String> getCommands() {
-        List<String> val = new ArrayList<String>();
-        val.add(CREATE_NETWORK);
-        return val;
-    }
-
-    public List<String> getArguments(String command) {
-        List<Tunable> list = settings.get(command);
-        if (list == null) return null;
-        List<String> args = new ArrayList<String>();
-        for (Tunable t : list) {
-            args.add(t.getName());
-        }
-        return args;
-    }
-
-    public Map<String, Object> getSettings(String command) {
-        List<Tunable> list = settings.get(command);
-        Map<String, Object> val = new HashMap<String, Object>();
-        for (Tunable t: list) {
-            val.put(t.getName(), t.getValue());
-        }
-        return val;
-    }
-
-	public String getDescription(String arg0) {
-		return "Set toggle to create network and view from imported nodes";
-	}
-
-
-    public Map<String, Tunable> getTunables(String command) {
-        Map<String, Tunable> map = new HashMap<String, Tunable>();
-        List<Tunable> list = settings.get(command);
-        for (Tunable t : list) {
-            map.put(t.getName(), t);
-        }
-        return map;
-    }
-
 }
+
